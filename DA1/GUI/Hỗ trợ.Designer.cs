002d0874adc334f5df250e1c4ab527958e0e9321
khang123hy <duykhang02vnn@gmail.com>
2023-05-02 21:35:01 +0700

    add hotro

---
+++ DA1/GUI/Hỗ trợ.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace GUI
+{
+    partial class Hỗ_trợ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(34, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1415, 723);
+            this.panel1.TabIndex = 0;
+            // 
+            // gunaElipse1
+            // 
+            this.gunaElipse1.Radius = 25;
+            this.gunaElipse1.TargetControl = this.panel1;
+            // 
+            // Hỗ_trợ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GUI.Properties.Resources.Nền2;
+            this.ClientSize = new System.Drawing.Size(1484, 787);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Hỗ_trợ";
+            this.Text = "Hỗ_trợ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI.WinForms.GunaElipse gunaElipse1;
+    }
+}
